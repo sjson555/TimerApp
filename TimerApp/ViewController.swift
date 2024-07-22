4e1b0b42df73385c781211023c0f68f6d8a1d7d9
sungjun son <o_nj_u@naver.com>
2024-07-23 00:30:57 +0900

    TimerApp 코드 설정

--- TimerApp/ViewController.swift
+++ TimerApp/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mainLabel: UILabel!
     
+    @IBOutlet weak var slider: UISlider!
     
     
     override func viewDidLoad() {
@@ -21,17 +22,26 @@ class ViewController: UIViewController {
 
     func configureUI() {
         mainLabel.text = "초를 선택하세요"
+        // 슬라이더 가운데 설정
+        slider.value = 0.5
     }
 
     @IBAction func sliderChanged(_ sender: UISlider) {
-        
+        // 슬라이더의 밸류값이 메인레이블에 텍스트로 나와야 함
+        // 슬라이더 밸류값(Float)-> 정수(Int)
+        let seconds = Int(slider.value * 60) // Float 0.0 ~ 1.0
+        mainLabel.text = "\(seconds)초"
     }
     
     @IBAction func startButtonTapped(_ sender: UIButton) {
-        
+        // 1초 마다 무언가 실행되어야 함
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
+        // 초기화 세팅
+        mainLabel.text = "초를 선택하세요"
+        // 슬라이더 가운데 설정
+        slider.setValue(0.5, animated: true)
         
     }
     
